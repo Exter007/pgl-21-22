5eabdf5365900eac02c9774975f9eefcbea1891f
Exter007 <externos.blops@gmail.com>
2022-03-16 11:12:45 +0100

    Part 2 : Adding some methods in CustomerController & FinancialProductRepository

--- financial_portfolio_server/src/main/java/com/pgl/repositories/FinancialProductRepository.java
+++ financial_portfolio_server/src/main/java/com/pgl/repositories/FinancialProductRepository.java
@@ -1,7 +1,13 @@
 package com.pgl.repositories;
 
 import com.pgl.models.FinancialProduct;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface FinancialProductRepository extends CrudRepository<FinancialProduct, Long> {
+    @Query("SELECT r FROM FinancialProduct r where r.financialInstitution.BIC=:f")
+    List<FinancialProduct> findAllByFinancialInstitution_BIC(@Param("f") String bic);
 }
